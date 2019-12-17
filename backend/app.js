0feae03e06afa63994b22d181ccbb22cbfddad9b
ubunturifa <jstrfaheem065@gmail.com>
2019-12-17 17:49:04 +0530

    OCR,

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 const path = require('path');
 const Result = require('./models/result');
+const tesseract = require('tesseract.js');
 
 const app = express();
 const multer = require('multer');
@@ -18,21 +19,38 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 
 app.use('/images', express.static(path.join('backend/images')));
+app.use('/tempimg', express.static(path.join('backend/tempimg')));
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
+    destination: (req, file, cb) => {
         const isValid = MIME_TYPE_MAP[file.mimetype];
         let error = null;
-        if(!isValid){
+        if (!isValid) {
             error = 'INVALID FILE TYPE!';
         }
 
         cb(error, 'backend/images');
     },
-    filename: (req, file, cb)=>{
+    filename: (req, file, cb) => {
         const name = file.originalname.toLowerCase().split(' ').join('-');
         const ext = MIME_TYPE_MAP[file.mimetype];
         cb(null, name + '-' + Date.now() + '.' + ext);
+    }
+});
+
+const tempstorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = null;
+        if (!isValid) {
+            error = 'INVALID FILE TYPE!';
+        }
+
+        cb(error, 'backend/tempimg');
+    },
+    filename: (req, file, cb) => {
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, 'tempimg.' + ext);
     }
 });
 
@@ -51,7 +69,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept');
-        res.setHeader('Access-Control-Allow-Method',
+    res.setHeader('Access-Control-Allow-Method',
         'GET, POST, PATCH, DELETE, OPTIONS');
     next();
 });
@@ -80,7 +98,7 @@ app.get('/api/users', (req, res, next) => {
     // We don't use params!
     // console.log(req.query);
     // https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
-    User.findOne({ user_name: req.query.user_name, password: req.query.password}).then((user) => {
+    User.findOne({ user_name: req.query.user_name, password: req.query.password }).then((user) => {
         if (user) {
             return res.status(200).json({ message: "CONGRATULATIONS!", id: user._id });
             // console.log(user);
@@ -89,36 +107,47 @@ app.get('/api/users', (req, res, next) => {
     });
 });
 
-app.delete('/api/users/', (req, res, next)=>{
-    User.deleteOne({_id: req.query.id, user_name: req.query.user_name, password: req.query.password}).then(del=>{
-        if(del.n){
-            return res.status(201).send({message: '201K, SUCCESS!'})
+app.delete('/api/users/', (req, res, next) => {
+    User.deleteOne({ _id: req.query.id, user_name: req.query.user_name, password: req.query.password }).then(del => {
+        if (del.n) {
+            return res.status(201).send({ message: '201K, SUCCESS!' })
         }
-        res.status(404).send({message: '404, NOT FOUND!'});
+        res.status(404).send({ message: '404, NOT FOUND!' });
     });
 });
 
-app.put('/api/users', (req, res, next)=>{
+app.put('/api/users', (req, res, next) => {
     User.updateOne();
 });
 
-app.post('/api/results', multer({storage: storage}).single('image'), (req, res, next)=>{
+app.post('/api/result', multer({storage: tempstorage}).single('image') ,(req, res, next) => {
+    const url = req.protocol + '://' + req.get('host');
+    let imagePath = url + '/tempimg/' + req.file.filename;
+    tesseract.recognize(imagePath, 'eng').then(({data : {text}})=>{
+        res.json({message: 'SUCCESS!',image_text: text});
+    });
+});
+
+app.post('/api/results', multer({ storage: storage }).single('image'), (req, res, next) => {
     const url = req.protocol + '://' + req.get('host');
     // console.log(req.body.result);
     let imagePath = url + '/images/' + req.file.filename;
     // console.log(imagePath);
+    // tesseract.recognize(imagePath, 'eng').then(({data : {text}})=>{
+    //     console.log(text);
+    // });
     const result = new Result({
         image_path: imagePath,
-        image_text: req.body.result 
+        image_text: req.body.result
     });
-    result.save().then(result=>{
+    result.save().then(result => {
         res.json({});
     });
 });
 
-app.get('/api/results', (req, res, next)=>{
-    Result.find().then(results=>{
-        console.log(results);
+app.get('/api/results', (req, res, next) => {
+    Result.find().then(results => {
+        // console.log(results);
         res.json(results);
     });
 });
