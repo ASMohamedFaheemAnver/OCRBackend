9e7b90d04ddb18c6362b7caf3c810e4de260bbfc
ubunturifa <jstrfaheem065@gmail.com>
2019-12-31 11:25:25 +0530

    Bcrypt and webtoken,

--- backend/router/user.js
+++ backend/router/user.js
@@ -1,38 +1,66 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/api/users', (req, res, next) => {
-
     User.findOne({ user_name: req.body.user_name }).then((ures) => {
         if (!ures) {
-            const user = new User({
-                user_name: req.body.user_name,
-                password: req.body.password
-            });
-            user.save();
-            // console.log(user);
-            return res.status(201).json({
-                message: '201K SUCCESS!',
-                user: user
+            return bcrypt.hash(req.body.password, 10).then(hash => {
+                const user = new User({
+                    user_name: req.body.user_name,
+                    password: hash
+                });
+                user.save().then(res0=>{
+                    return res.status(201).json({
+                        message: '201K SUCCESS!',
+                        user: res0
+                    });
+                });
+                // console.log(user);
+            }).catch(err => {
+                console.log(err);
+                res.status(500).json({ error: err });
             });
         }
-
         res.status(403).json({ message: "ALREADY A USER EXIST!" });
     });
 });
 
 router.get('/api/users', (req, res, next) => {
+    // console.log(req.query);
     // We don't use params!
     // console.log(req.query);
     // https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js
-    User.findOne({ user_name: req.query.user_name, password: req.query.password }).then((user) => {
-        if (user) {
-            return res.status(200).json({ message: "CONGRATULATIONS!", id: user._id });
+    let oUser;
+    User.findOne({ user_name: req.query.user_name }).then((user) => {
+        if (!user) {
+            return res.status(404).json({ message: "USER NOT FOUND!" });
             // console.log(user);
         }
-        res.status(404).json({ message: "USER NOT FOUND!" });
-    });
+
+        oUser = user;
+        return bcrypt.compare(req.query.password, user.password);
+    }).then(res0 => {
+        if (!res0) {
+            return res.status(401).json({
+                message: 'AUTH FAILED!'
+            });
+        }
+        const token = jwt.sign({ email: oUser.email, userId: oUser._id },
+            'i_type_rifa_to_secure_my_token',
+            { expiresIn: '1h' });
+            // console.log(token);
+        res.status(200).json({
+            token: token,
+            expiresIn: 3600,
+            id: oUser._id
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+    });;
 });
 
 router.get('/api/user_id', (req, res, next) => {
